20a2844ad3ebdf7664bf1aa38f6c0582e46fc16c
ryo310jp <mr.310.ryo@gmail.com>
2022-08-04 12:12:54 +0900

    create table

---
+++ scripts/create_table.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+func main() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	client := dynamodb.NewFromConfig(cfg)
+	createTable(client, "quick-photos")
+}
+
+func createTable(client *dynamodb.Client, name string) error {
+	describeIn := dynamodb.DescribeTableInput{
+		TableName: aws.String(name),
+	}
+	_, err := client.DescribeTable(context.TODO(), &describeIn)
+	if err == nil {
+		// table exists
+		return nil
+	}
+
+	in := dynamodb.CreateTableInput{
+		TableName: aws.String(name),
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("PK"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+			{
+				AttributeName: aws.String("SK"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("PK"),
+				KeyType:       types.KeyTypeHash,
+			},
+			{
+				AttributeName: aws.String("SK"),
+				KeyType:       types.KeyTypeRange,
+			},
+		},
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+	}
+
+	if _, err := client.CreateTable(context.TODO(), &in); err != nil {
+		return err
+	}
+
+	return nil
+}
